3a490100fd3d2bb3fae0c2e685fcf6d4f852d49b
erika mayoute <e.mayoute44@gmail.com>
2022-06-07 12:08:21 +0200

    Add static pages

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'public/home'
+  get 'public/about'
+  get 'public/legal'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "public#home"
+end
