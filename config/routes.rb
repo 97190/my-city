99d4ae0697c4c206d3d14454926e113c2d2095b5
erika mayoute <e.mayoute44@gmail.com>
2022-06-08 12:18:08 +0200

    begin install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'public/home'
+  # get 'public/home'
   get 'public/about'
   get 'public/legal'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
