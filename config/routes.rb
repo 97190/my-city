4533d13664014339bd9609059d94677d3b13fd8d
erika mayoute <e.mayoute44@gmail.com>
2022-06-09 15:37:59 +0200

    ✔ Navbar functionnel

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  # get 'public/home'
+
+  get 'public/home'
   get 'public/about'
   get 'public/legal'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
